4390de0a76644ff921806f9c7efd4ac56a64b5e3
Konstantin Volodin <volodin.konstantin@gmail.com>
2016-11-02 09:41:33 +0300

    Update postgresql.sh

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# add postgresql 
+# качаем PostgreSQL
 wget https://ftp.postgresql.org/pub/source/v9.5.3/postgresql-9.5.3.tar.gz
 
-# extract archive content	
+# разархивируем в папку 	
 tar -xzvf postgresql-9.5.3.tar.gz
 rm -f postgresql-9.5.3.tar.gz
     
-# compile postgresql
+# компилируем postgresql
 cd postgresql-9.5.3
 ./configure --prefix=/usr/local/pgsql
 sleep 5
@@ -16,13 +16,14 @@ sleep 5
 make check
 sleep 5
 	
-# install it
+# время установить PosgreSQL в систему
 sudo make install-world
 	
-# add postgres user
+# добавляем пользователя postgres с паролем postgres
 echo "adding postgres user. password: postgres"
 sudo useradd -p .7BkIrW3xJCN. -m -d /home/postgres postgres
     	
+# правим права доступа
 echo "mkdir & chown"
 sudo mkdir /usr/local/pgsql/data
 sudo chown postgres /usr/local/pgsql/data
@@ -30,11 +31,11 @@ sudo chown postgres /usr/local/pgsql/data
 # init base cluster
 sudo su -c "/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data" postgres
     	
-# copy config files
+# копируем файлы конфигурации
 sudo cp /vagrant/conf/pg_hba.conf /usr/local/pgsql/data
 sudo cp /vagrant/conf/postgresql.conf /usr/local/pgsql/data
 
-# add paths
+# добавляем в переменные окружения пути и переменные
 sudo su -c "echo '#postgres paths' >> ~/.profile" postgres
 sudo su -c "echo 'export PATH=/usr/local/pgsql/bin:/usr/local/pgsql/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' >> ~/.profile" postgres
 sudo su -c "echo 'export PGDATA=/usr/local/pgsql/data' >> ~/.profile" postgres
